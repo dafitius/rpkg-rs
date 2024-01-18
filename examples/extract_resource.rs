29e482c99c8e164ffb1bcf01ae65b111da93b4ab
David <davidstulemeijer@gmail.com>
2024-01-18 13:33:43 +0100

    Moved the resource extraction logic to the ResourcePackage struct

--- examples/extract_resource.rs
+++ examples/extract_resource.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io;
 use std::io::{Cursor, Read, Seek};
 use std::path::PathBuf;
+use std::str::Utf8Error;
 use binrw::BinReaderExt;
 use lz4::block::decompress_to_buffer;
 use memmap2::Mmap;
@@ -15,69 +16,35 @@ fn main() {
     let package_path = PathBuf::from("S:/Steam/steamapps/common/Hitman™/Runtime/chunk0.rpkg");
     //let package_path = PathBuf::from("D:/Steam/steamapps/common/HITMAN 3/Runtime/chunk0.rpkg");
     let rid = ResourceID::from_string("[assembly:/repository/pro.repo].pc_repo");
-    let rrid : RuntimeResourceID = RuntimeResourceID::from_resource_id(&rid);
+    let rrid: RuntimeResourceID = RuntimeResourceID::from_resource_id(&rid);
 
     //parse the ResourcePackage
-    match File::open(&package_path){
+    match File::open(&package_path) {
         Ok(file) => {
             let mmap = unsafe { Mmap::map(&file).unwrap() };
             let mut reader = Cursor::new(&mmap[..]);
             let is_patch = package_path.clone().file_name().unwrap().to_str().unwrap().contains("patch");
-            let rpkg: ResourcePackage = reader.read_ne_args((is_patch,)).unwrap();
-
-            //try to find the resource inside the package
-            if let Some((resource_header, resource_offset_info)) = rpkg
-                .resource_entries
-                .iter()
-                .enumerate()
-                .find(|(_, entry)| entry.runtime_resource_id == rrid)
-                .map(|(index, entry)| (rpkg.resource_metadata.get(index).unwrap(), entry))
-            {
-                println!("I want to extract: {}", resource_header);
-                println!("Let's extract that from: {}", resource_offset_info);
-
-                let final_size = resource_offset_info.compressed_size_and_is_scrambled_flag & 0x3FFFFFFF;
-                let is_lz4ed = final_size != resource_header.data_size;
-                let is_scrambled = resource_offset_info.compressed_size_and_is_scrambled_flag & 0x80000000 == 0x80000000;
-
-                // Extract the resource bytes from the resourcePackage
-                match File::open(&package_path){
-                    Ok(mut file) =>{
-                        file.seek(io::SeekFrom::Start(resource_offset_info.data_offset)).unwrap();
-                        let mut buffer = vec![0; final_size as usize];
-                        file.read_exact(&mut buffer).unwrap();
-
-                        if is_scrambled {
-                            let str_xor = vec![0xdc, 0x45, 0xa6, 0x9c, 0xd3, 0x72, 0x4c, 0xab];
-                            buffer = buffer
-                                .iter()
-                                .enumerate()
-                                .map(|(index, byte)| byte ^ str_xor[index % str_xor.len()])
-                                .collect();
-                        }
-
-                        let mut file = vec![0; resource_header.data_size as usize];
-
-                        if is_lz4ed {
-                            let size = decompress_to_buffer(&*buffer, Some(resource_header.data_size as i32), &mut *file).expect("TODO: panic message");
-                            if size == resource_header.data_size as usize {
-                                println!("{:?}", std::str::from_utf8(&*file));
-                            }
-                        }
-                        else{
-                            println!("{:?}", buffer.iter());
-                        }
-                    },
-                    Err(e) =>{
-                        eprintln!("There was an error opening the file: {}", e);
-                    }
-                };
-
-            } else {
-                eprintln!("Couldn't find the requested resource inside of the given resource package");
+            let rpkg: ResourcePackage = reader.read_ne_args((is_patch, )).unwrap_or_else(|e| {
+                println!("Failed to parse package: {}", e);
+                std::process::exit(1)
             }
-        },
-        Err(e) =>{
+            );
+
+            let file = rpkg.get_resource(&package_path, &rrid).unwrap_or_else(|e| {
+                println!("Failed extract resource: {}", e);
+                std::process::exit(1)
+            });
+
+            match std::str::from_utf8(&*file) {
+                Ok(s) => {
+                    println!("{}...", s.chars().take(100).collect::<String>())
+                }
+                Err(e) => {
+                    println!("first bytes: {:?}", file.iter().take(50).collect::<Vec<_>>());
+                }
+            };
+        }
+        Err(e) => {
             eprintln!("There was an error opening the file: {}", e);
         }
     }
--- src/runtime/resource/resource_package.rs
+++ src/runtime/resource/resource_package.rs
@@ -1,26 +1,39 @@
-use std::fmt;
+use std::{fmt, io};
+use std::fs::File;
+use std::io::{Read, Seek};
+use std::path::{Path, PathBuf};
 use binrw::BinRead;
+use lz4::block::decompress_to_buffer;
 use modular_bitfield::prelude::*;
+use thiserror::Error;
 
 use crate::runtime::resource::runtime_resource_id::RuntimeResourceID;
 
+#[derive(Debug, Error)]
+pub enum ResourcePackageError {
+    #[error("Error opening the file: {0}")]
+    IoError(#[from] std::io::Error),
+
+    #[error("Couldn't find the requested resource inside of the given resource package")]
+    ResourceNotFound,
+
+}
 
 #[allow(dead_code)]
 #[derive(BinRead)]
 #[br(import(is_patch: bool))]
 pub struct ResourcePackage {
-
     pub magic: [u8; 4],
 
-    #[br(if(magic == *b"2KPR"))]
+    #[br(if (magic == * b"2KPR"))]
     pub metadata: Option<PackageMetadata>,
 
     pub header: PackageHeader,
 
-    #[br(if(is_patch && metadata.as_ref().map_or(false, |m| m.patch_id > 0)))]
+    #[br(if (is_patch && metadata.as_ref().map_or(false, | m | m.patch_id > 0)))]
     unneeded_resource_count: u32,
 
-    #[br(if(is_patch))]
+    #[br(if (is_patch))]
     #[br(little, count = unneeded_resource_count)]
     pub unneeded_resources: Option<Vec<RuntimeResourceID>>,
 
@@ -29,6 +42,48 @@ pub struct ResourcePackage {
 
     #[br(little, count = header.file_count)]
     pub resource_metadata: Vec<ResourceHeader>,
+}
+
+impl ResourcePackage {
+    pub fn get_resource(&self, package_path: &PathBuf, rrid: &RuntimeResourceID) -> Result<Vec<u8>, ResourcePackageError> {
+        let (resource_header, resource_offset_info) = self
+            .resource_entries
+            .iter()
+            .enumerate()
+            .find(|(_, entry)| entry.runtime_resource_id == *rrid)
+            .map(|(index, entry)| (self.resource_metadata.get(index).unwrap(), entry)).ok_or_else(|| ResourcePackageError::ResourceNotFound)?;
+
+        let final_size = resource_offset_info.get_compressed_size();
+        let is_lz4ed = final_size != resource_header.data_size;
+        let is_scrambled = resource_offset_info.get_is_scrambled();
+
+        // Extract the resource bytes from the resourcePackage
+        let mut file = File::open(&package_path).map_err(ResourcePackageError::IoError)?;
+
+        file.seek(io::SeekFrom::Start(resource_offset_info.data_offset)).unwrap();
+
+        let mut buffer = vec![0; final_size as usize];
+        file.read_exact(&mut buffer).unwrap();
+
+        if is_scrambled {
+            let str_xor = vec![0xdc, 0x45, 0xa6, 0x9c, 0xd3, 0x72, 0x4c, 0xab];
+            buffer.iter_mut().enumerate().for_each(|(index, byte)| {
+                *byte ^= str_xor[index % str_xor.len()];
+            });
+        }
+
+        if is_lz4ed {
+            let mut file = vec![0; resource_header.data_size as usize];
+            let size = decompress_to_buffer(&buffer, Some(resource_header.data_size as i32), &mut file)
+                .map_err(ResourcePackageError::IoError)?;
+
+            if size == resource_header.data_size as usize {
+                return Ok(file);
+            }
+        }
+
+        return Ok(buffer);
+    }
 }
 
 #[allow(dead_code)]
@@ -40,9 +95,8 @@ pub struct PackageMetadata {
     pub patch_id: u8,
 
     pub unknown1: u8,
-    pub unknown2: u8
+    pub unknown2: u8,
 }
-
 
 #[allow(dead_code)]
 #[derive(BinRead)]
@@ -60,33 +114,32 @@ pub struct PackageOffsetInfo {
     pub compressed_size_and_is_scrambled_flag: u32,
 }
 
-impl PackageOffsetInfo{
+impl PackageOffsetInfo {
     pub fn get_is_scrambled(&self) -> bool
-    { self.compressed_size_and_is_scrambled_flag & 0x80000000 == 0x80000000}
+    { self.compressed_size_and_is_scrambled_flag & 0x80000000 == 0x80000000 }
 
     pub fn get_compressed_size(&self) -> u32
-    { self.compressed_size_and_is_scrambled_flag & 0x7FFFFFFF}
+    { self.compressed_size_and_is_scrambled_flag & 0x7FFFFFFF }
 }
 
-
 #[allow(dead_code)]
-#[cfg_attr(feature="serde", derive(serde::Serialize))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize))]
 #[derive(BinRead)]
 pub struct ResourceHeader
 {
-    pub m_type : [u8; 4],
+    pub m_type: [u8; 4],
     pub references_chunk_size: u32,
     pub states_chunk_size: u32,
     pub data_size: u32,
     pub system_memory_requirement: u32,
     pub video_memory_requirement: u32,
 
-    #[br(if(references_chunk_size > 0))]
-    pub m_references : Option<ResourceReferences>
+    #[br(if (references_chunk_size > 0))]
+    pub m_references: Option<ResourceReferences>,
 }
 
 #[allow(dead_code)]
-#[cfg_attr(feature="serde", derive(serde::Serialize))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize))]
 #[derive(BinRead)]
 pub struct ResourceReferences
 {
@@ -100,7 +153,7 @@ pub struct ResourceReferences
 }
 
 #[bitfield]
-#[cfg_attr(feature="serde", derive(serde::Serialize))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize))]
 #[derive(BinRead)]
 #[br(map = Self::from_bytes)]
 pub struct ResourceReferenceFlags
@@ -117,9 +170,9 @@ pub struct ResourceReferenceFlags
 #[bits = 2]
 pub enum ReferenceType
 {
-    INSTALL = 0 ,
-    NORMAL = 1 ,
-    WEAK = 2
+    INSTALL = 0,
+    NORMAL = 1,
+    WEAK = 2,
 }
 
 impl fmt::Display for PackageOffsetInfo {
@@ -130,9 +183,8 @@ impl fmt::Display for PackageOffsetInfo {
 
 impl fmt::Display for ResourceHeader {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-
         let mut res_type = self.m_type;
         res_type.reverse();
-        write!(f, "type: {}, reference_num: {}, size: {}, num_reqs: ({} {})", std::str::from_utf8(&res_type).unwrap() , self.references_chunk_size, self.data_size, self.system_memory_requirement, self.video_memory_requirement)
+        write!(f, "type: {}, reference_num: {}, size: {}, num_reqs: ({} {})", std::str::from_utf8(&res_type).unwrap(), self.references_chunk_size, self.data_size, self.system_memory_requirement, self.video_memory_requirement)
     }
 }
