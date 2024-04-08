90d1e97c7624b9104bffc8b2274c40084eca7b72
David <davidstulemeijer@gmail.com>
2024-04-08 16:55:27 +0200

    fixed a minor issue

--- examples/mount_game_files.rs
+++ examples/mount_game_files.rs
@@ -25,10 +25,7 @@ fn main() {
         std::process::exit(1);
     });
 
-    let app_options = &thumbs.get_root().unwrap_or_else(|| {
-        eprintln!("Missing root in thumbs file");
-        std::process::exit(1);
-    })["application"];
+    let app_options = &thumbs.get_root()["application"];
 
     if let (Some(proj_path), Some(relative_runtime_path)) = (app_options.get_option("PROJECT_PATH"), app_options.get_option("RUNTIME_PATH")) {
 
