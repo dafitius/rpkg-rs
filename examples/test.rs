499ba9d3bdd1fbb2ce187b66ee6a80cab11ca6ec
dafitius <70489995+dafitius@users.noreply.github.com>
2024-04-30 16:36:39 +0200

    Delete examples/test.rs

--- examples/test.rs
+++
@@ -1,8 +0,0 @@
-use rpkg_rs::resource::runtime_resource_id::RuntimeResourceID;
-
-pub fn main() {
-    let resource_id = RuntimeResourceID::from(0x00097AA87B144150);
-
-    let rrid_str = serde_json::to_string(&resource_id);
-    println!("{:?}", rrid_str);
-}
